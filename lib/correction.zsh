bf38d1ac32d513c84b570e1c74719a4885f1d26f
Drew Nall <drewnall@gmail.com>
2013-03-23 01:39:20 -0400

    Removed some stuff I don't use.

--- lib/correction.zsh
+++ lib/correction.zsh
@@ -8,7 +8,5 @@ else
   alias mkdir='nocorrect mkdir'
   alias gist='nocorrect gist'
   alias heroku='nocorrect heroku'
-  alias ebuild='nocorrect ebuild'
-  alias hpodder='nocorrect hpodder'
-  alias sudo='nocorrect sudo'
+ alias sudo='nocorrect sudo'
 fi
