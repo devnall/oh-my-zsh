6372593b0c0d3a41884e515d440b494101a229d8
Drew Nall <drewnall@gmail.com>
2013-03-24 14:00:13 -0400

    Added a bunch of custom aliases.

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -16,11 +16,32 @@ alias g='grep -in'
 alias history='fc -l 1'
 
 # List direcory contents
-alias lsa='ls -lah'
+alias lsa='ls -lahF'
 alias l='ls -lA1'
 alias ll='ls -l'
 alias la='ls -lA'
-alias sl=ls # often screw this up
+
+alias rm='rm -i'
+alias du='du -h'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
+alias cl='clear'
+
+alias cp='cp -i'
+alias mv='mv -i'
+
+alias tf='tail -f'
+
+alias ssr='ssh -l root'
+
+alias gvim='/usr/local/bin/mvim'
 
 alias afind='ack-grep -il'
 
+if [[ `which cdf` == '/usr/local/bin/cdf' ]]; then
+  alias df='cdf -h'
+else
+  alias df='df -h'
+fi
