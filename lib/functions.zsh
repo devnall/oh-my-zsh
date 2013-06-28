578199988177a934a4897d867de40a80b6216d41
Drew Nall <devnall@gmail.com>
2013-06-28 15:51:14 -0400

    Add new dusk and duck functions to produce human readable, sorted du output

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -15,3 +15,26 @@ function take() {
   cd $1
 }
 
+function dusk() {
+  du -sk "$@" | sort -n | while read size fname 
+    do for unit in k M G T P E Z Y
+      do if [ $size -lt 1024 ]
+        then echo -e "${size}${unit}\t${fname}"
+        break
+      fi
+    size=$((size/1024))
+    done
+  done
+}
+
+function duck() {
+  du -ck "$@" | sort -n | while read size fname 
+    do for unit in k M G T P E Z Y
+      do if [ $size -lt 1024 ]
+        then echo -e "${size}${unit}\t${fname}"
+        break
+      fi
+    size=$((size/1024))
+    done
+  done
+}
