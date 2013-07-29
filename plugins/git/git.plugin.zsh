ea6503f977e6aa5fda70f30339f84d1d91b4711b
Drew Nall <devnall@gmail.com>
2013-07-29 12:00:33 -0400

    Make glg git log output pretty

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -52,8 +52,11 @@ compdef gcount=git
 alias gcl='git config --list'
 alias gcp='git cherry-pick'
 compdef _git gcp=git-cherry-pick
-alias glg='git log --stat --max-count=5'
+#alias glg='git log --stat --max-count=5'
+#compdef _git glg=git-log
+alias glg='git log --pretty=format"%h %ad | %s%d [%an]" --graph --date=short'
 compdef _git glg=git-log
+##
 alias glgg='git log --graph --max-count=5'
 compdef _git glgg=git-log
 alias glgga='git log --graph --decorate --all'
